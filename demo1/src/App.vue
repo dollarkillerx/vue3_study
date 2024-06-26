4635f213db03774d87f6a1693ae86e5d35ad4ea6
Леони́д Ильи́ч Бре́жнев <adapawang@gmail.com>
2024-06-26 16:21:42 +0900

    init demo1

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -3,6 +3,7 @@ import {RouterLink, RouterView} from 'vue-router'
 import ItemList from './components/Items/Item.components.vue'
 import type {ItemInterface} from "@/models";
 import {ref, reactive, computed} from 'vue'
+import request from "@/request/request";
 
 let count = ref(0);
 
@@ -34,6 +35,20 @@ const completedAll = computed(()=>{
 
   return countCompleted == all ? 'All todo is completed' : 'No';
 });
+
+interface Root {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
+const clickPs = async () => {
+  const data: Root = await request.get<Root>("/todos/1");
+  console.log(data.userId);
+  console.log(data.title);
+  console.log(data.completed);
+};
 </script>
 
 <template>
@@ -41,6 +56,8 @@ const completedAll = computed(()=>{
     <h1>My Todo List</h1>
     <h3>{{completedAll}}</h3>
 
+
+    <button @click="clickPs">Click</button>
     <ItemList :Items="Items" @changeItem="CompletedItem"></ItemList>
 
     {{Items}}
---
+++ demo1/src/config/index.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+    apiURI: "https://jsonplaceholder.typicode.com",
+    authToken: "qd7jGtUgkC",
+    timeout: 3000, // Milliseconds
+};

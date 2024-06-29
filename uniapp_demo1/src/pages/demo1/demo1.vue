1c7d447ee00ef509d123ceedcca73ae06a82c3cb
Леони́д Ильи́ч Бре́жнев <adapawang@gmail.com>
2024-06-29 16:31:35 +0900

    add tailwind css

---
+++ uniapp_demo1/src/pages/demo1/demo1.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="font-mono">
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- uniapp_demo1/src/pages/index/index.vue
+++ uniapp_demo1/src/pages/index/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+const toLogin = () => {
+  uni.navigateTo({
+    url: '/pages/demo1/demo1',
+  });
+}
 </script>
 
 <template>
-this is index
+<div>
+  <button class="bg-yellow-500" @click="toLogin">demo1</button>
+</div>
 </template>
 
 <style scoped>
